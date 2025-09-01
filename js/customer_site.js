8634d4da8faaa22195b348cf1d6099d4ebb6df84
Aaron <aglaura@msn.com>
2025-09-01 15:15:46 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,7 +2,7 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
-    }，
+    },
     zuid: {
         api: 'https://api.zuidapi.com/api.php/provide/vod',
         name: '最大资源',
